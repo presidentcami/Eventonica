4d460e1f2514654dfa3f4e0ed203fc4ffe5092ce
Camille Williams <camiwills325@gmail.com>
2023-03-15 10:54:40 -0400

    Add input fields and button for new event form

--- client/src/components/newEventForm.js
+++ client/src/components/newEventForm.js
@@ -1,7 +1,34 @@
 
 const NewEventForm = () => {
     return (
-        <div>New Form</div>
+        <div>
+            <form>
+                <label>Event Title</label>
+                <input type="text" id="add-event-title"
+                    placeholder="The Title of your Event"
+                    required
+                    // value={event.title} //state
+                    // onChange={handleTitleChange}
+                />
+                <label>Place</label>
+                <input
+                    type="text"
+                    id="add-event-location"
+                    placeholder="The Location of your Event"
+                    required
+                    // value={event.location} //state
+                    // onChange={handleLocationChange}
+                />
+                <label>Date</label>
+                <input
+                    type="date"
+                    id="add-event-date"
+                    // value={event.eventtime} //state
+                    // onChange={handleDateChange}
+                />
+                <button type="submit" >Add Event</button>
+            </form>
+        </div>
     )
 }
 
