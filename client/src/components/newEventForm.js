071c371452237c73c33114ac9b7639046be67f05
Camille Williams <camiwills325@gmail.com>
2023-03-16 12:14:46 -0400

    Set up form to take inputs with useReducer

--- client/src/components/newEventForm.js
+++ client/src/components/newEventForm.js
@@ -1,32 +1,70 @@
+import { useReducer } from 'react'
+
+    const initialValue = {
+        title: "",
+        location: "",
+        eventtime: "",
+    };
+
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case 'update': 
+            return {
+                ...state,
+                [action.payload.key]: action.payload.value,
+            };
+            default:
+                throw new Error(`Unknown action type: ${action.type}`);
+        }
+    };
 
 const NewEventForm = () => {
+
+    const [state, dispatch] = useReducer(reducer, initialValue);
+
+    const inputAction = (event) => {
+        event.preventDefault();
+        
+        dispatch({
+            type: 'update',
+            payload: { key: event.target.name, value: event.target.value },
+        });
+        
+    };
+
+// console.log("state", state)
     return (
         <div>
             <form>
                 <label>Event Title</label>
-                <input type="text" id="add-event-title"
-                    placeholder="The Title of your Event"
+                <input 
+                    type="text" 
+                    id="add-event-title"
+                    // placeholder="The Title of your Event"
                     required
-                    // value={event.title} //state
-                    // onChange={handleTitleChange}
+                    defaultValue={state.title} //state
+                    name="title"
+                    onChange={inputAction}
                 />
                 <label>Place</label>
                 <input
                     type="text"
                     id="add-event-location"
-                    placeholder="The Location of your Event"
+                    // placeholder="The Location of your Event"
                     required
-                    // value={event.location} //state
-                    // onChange={handleLocationChange}
+                    defaultValue={state.location} //state
+                    name="location"
+                    onChange={inputAction}
                 />
                 <label>Date</label>
                 <input
                     type="date"
                     id="add-event-date"
-                    // value={event.eventtime} //state
-                    // onChange={handleDateChange}
+                    defaultValue={state.eventtime} //state
+                    name="eventtime"
+                    onChange={inputAction}
                 />
-                <button type="submit" >Add Event</button>
+                <button type="submit">Add Event</button>
             </form>
         </div>
     )
